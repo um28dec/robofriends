95bdded7e55df2b9635ace3d6c0283a54ccfe417
Umang Raj <umangraj@Umangs-MacBook-Air.local>
2020-07-25 18:55:20 +0530

    Updates

---
+++ precache-manifest.77b57e962909bfe19c4f46fee4de5442.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04e7c03a5738adc04f46a474d446e400",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "6961e6f3506742fcdbc1",
+    "url": "/static/css/main.01cae5d8.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6961e6f3506742fcdbc1",
+    "url": "/static/js/main.c1d9e311.chunk.js"
+  },
+  {
+    "revision": "af1ee15f3b8733767315",
+    "url": "/static/js/runtime-main.8a8ac9cd.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
